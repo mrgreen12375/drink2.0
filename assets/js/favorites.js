78641b6779c232450027180ad86a60cce0cfb683
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2024-11-21 21:29:22 -0700

    added delete feature for favorite cards

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,41 +1,52 @@
-function favoritesList() {
-    var favorites = document.querySelector('#favorites');
+var favorites = document.querySelector('#favorites');
 
+var favoriteEl;
+
+var saved = [];
+
+function init() {
     var savedCocktail = JSON.parse(localStorage.getItem("savedCocktail"));
-
-    console.log(savedCocktail)
-
-    if (savedCocktail === null) {
-        var favoriteEl = document.createElement('div');
+    if (savedCocktail !== null) {
+      saved = savedCocktail;
+    }
+    favoritesList();
+  }
+  
+function favoritesList() {
+console.log(saved);
+    if (saved.length === 0) {
+        favoriteEl = document.createElement('div');
         favoriteEl.innerHTML = `<div class='favoriteCardEmpty'>
                                 <h1>No Favorite Cocktails Yet!</h1>
                                 </div>`;
 
         favorites.appendChild(favoriteEl);
     } else {
-        for (let i = savedCocktail.length - 1; i >= 0; i--){
+        for (let i = saved.length - 1; i >= 0; i--){
                     
-            var favoriteEl = document.createElement('div');
+            favoriteEl = document.createElement('div');
+            favoriteEl.setAttribute("data-index", i);
             favoriteEl.setAttribute('class', 'favoriteCard')
-            favoriteEl.innerHTML = `<h2>${savedCocktail[i].name}</h2>
-                                    <img src="${savedCocktail[i].image}"/>
-                                    <p>${savedCocktail[i].instructions}</p>`;
+            favoriteEl.innerHTML = `<button>X</button>
+                                    <h2>${saved[i].name}</h2>
+                                    <img src="${saved[i].image}"/>
+                                    <p>${saved[i].instructions}</p>`;
 
             favorites.appendChild(favoriteEl);
 
-            for (var j = 0; j < savedCocktail[i].ingredients.length; j++) {
+            for (var j = 0; j < saved[i].ingredients.length; j++) {
 
                 var ingredientList = document.createElement('li');
-                ingredientList.innerHTML = `${savedCocktail[i].measurements[j]} : ${savedCocktail[i].ingredients[j]}`;
+                ingredientList.innerHTML = `${saved[i].measurements[j]} : ${saved[i].ingredients[j]}`;
                
                 favoriteEl.appendChild(ingredientList);
             }
 
-            if (savedCocktail[i].video === null) {
+            if (saved[i].video === null) {
                 console.log("no video available");
             } else {
                 var videoEl = document.createElement('a');
-                videoEl.href = savedCocktail[i].video;
+                videoEl.href = saved[i].video;
                 videoEl.target = "_blank";
                 videoEl.textContent = "Video";
                 videoEl.classList.add("video");
@@ -49,4 +60,18 @@ function favoritesList() {
     }
 }
 
-favoritesList()
+function deleteSavedListItems(event) {
+    var element = event.target;
+  
+    if (element.matches("button") === true) {
+      var index = element.parentElement.getAttribute("data-index");
+      saved.splice(index, 1);
+      localStorage.setItem("savedCocktail", JSON.stringify(saved));
+      location.reload();
+    }
+  }
+
+favorites.addEventListener("click", deleteSavedListItems);
+
+init()
+
